c907052fa2639e64f5b8901089e34872b2e77cb9
yxtay2324 <56621503+yxtay2324@users.noreply.github.com>
2021-09-02 20:38:11 +0800

    Add files via upload

--- selenium_bbot.py
+++ selenium_bbot.py
@@ -1,4 +1,3 @@
-import multiprocessing
 import time as timer
 import tkinter as tk
 #from pathos.multiprocessing import ProcessPool
@@ -9,10 +8,10 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from pathos.helpers import freeze_support
+from selenium.common.exceptions import UnexpectedAlertPresentException
 
 chromedriver_path = r"C:\Users\Annihilat0R\Downloads\chromedriver_win32\chromedriver.exe"
-MIDNIGHT = "00:02:00"
+MIDNIGHT = "20:36:00"
 COURT_ORDER = [3, 4, 1, 2, 5, 6]
 COURT_TO_XPATH = {
     "1600-1700 court 1": "tr[50]/td[10]",
@@ -39,7 +38,8 @@ user_database = {
     "ZH": ["TANZ0154", "Buttosai1998!"],
     "CELESTE": ["ta0005te", "MHiss@CE230121"],
     "JOLENE": ["JOLE0009", "Bts1306!"],
-    "NIGEL": ["nleong003", "S765432e!"]
+    "NIGEL": ["nleong003", "S765432e!"],
+    "DYLAN": ["Dyeo018", "dalhamix8991!"] 
 }
 
 class App(tk.Frame):
@@ -89,10 +89,9 @@ class App(tk.Frame):
             user = user_database[self.user_listbox.get(self.user_listbox.curselection())]
         timing = self.timing_listbox.get(self.timing_listbox.curselection())
 
-
-        process_1 = Process(target=new_window, args=(user, 3, timing,))
-        process_2 = Process(target=new_window, args=(user, 2, timing,))
-        process_3 = Process(target=new_window, args=(user, 4, timing,))
+        process_1 = Process(target=new_window, args=(self.debugger_mode, user, 3, timing,))
+        process_2 = Process(target=new_window, args=(self.debugger_mode, user, 2, timing,))
+        process_3 = Process(target=new_window, args=(self.debugger_mode, user, 4, timing,))
 
         process_1.start()
         process_2.start()
@@ -110,17 +109,17 @@ class App(tk.Frame):
         # 1600-1700 tr[50-55], 1700-1800 tr[56-61], 1800-1900 tr[62-67]
         # empty courts have an additional final segment named "input" after the td[...], and must be included in the xpath for bbot to find the button
         # note that ALL court one columns extend to 10 columns, meaning td[10] must be used instead of td[9]
-        #example of taken court element-(1700-1800 court 1) "//*[@id='top']/div/section[2]/div/div/p/table/tbody/tr/td[2]/form/table[2]/tbody/tr[56]/td[10]"
-        #example of taken court element-(1800-1900 court 1) "//*[@id="top"]/div/section[2]/div/div/p/table/tbody/tr/td[2]/form/table[2]/tbody/tr[62]/td[10]"
-        #example of empty court element-(1900-2000 court 1) "//*[@id="top"]/div/section[2]/div/div/p/table/tbody/tr/td[2]/form/table[2]/tbody/tr[68]/td[10]/input"
-        #example of taken court element-(1600-1700 court 2) "//*[@id="top"]/div/section[2]/div/div/p/table/tbody/tr/td[2]/form/table[2]/tbody/tr[51]/td[9]"
-        #example of taken court element-(1600-1700 court 3) "//*[@id="top"]/div/section[2]/div/div/p/table/tbody/tr/td[2]/form/table[2]/tbody/tr[52]/td[9]"
-        #example of taken court element-(1700-1800 court 3) "//*[@id="top"]/div/section[2]/div/div/p/table/tbody/tr/td[2]/form/table[2]/tbody/tr[58]/td[9]"
-        #example of taken court element-(1800-1900 court 3) "//*[@id="top"]/div/section[2]/div/div/p/table/tbody/tr/td[2]/form/table[2]/tbody/tr[64]/td[9]"
+        # example of taken court element-(1700-1800 court 1) "//*[@id='top']/div/section[2]/div/div/p/table/tbody/tr/td[2]/form/table[2]/tbody/tr[56]/td[10]"
+        # example of taken court element-(1800-1900 court 1) "//*[@id="top"]/div/section[2]/div/div/p/table/tbody/tr/td[2]/form/table[2]/tbody/tr[62]/td[10]"
+        # example of empty court element-(1900-2000 court 1) "//*[@id="top"]/div/section[2]/div/div/p/table/tbody/tr/td[2]/form/table[2]/tbody/tr[68]/td[10]/input"
+        # example of taken court element-(1600-1700 court 2) "//*[@id="top"]/div/section[2]/div/div/p/table/tbody/tr/td[2]/form/table[2]/tbody/tr[51]/td[9]"
+        # example of taken court element-(1600-1700 court 3) "//*[@id="top"]/div/section[2]/div/div/p/table/tbody/tr/td[2]/form/table[2]/tbody/tr[52]/td[9]"
+        # example of taken court element-(1700-1800 court 3) "//*[@id="top"]/div/section[2]/div/div/p/table/tbody/tr/td[2]/form/table[2]/tbody/tr[58]/td[9]"
+        # example of taken court element-(1800-1900 court 3) "//*[@id="top"]/div/section[2]/div/div/p/table/tbody/tr/td[2]/form/table[2]/tbody/tr[64]/td[9]"
         # confirm button element- //*[@id="top"]/div/section[2]/div/div/p/table/tbody/tr/td[2]/form/input[18]
        
 
-def new_window(username, court_number, timing):
+def new_window(debug, username, court_number, timing):
     driver = webdriver.Chrome(chromedriver_path) 
     driver.get('https://sso.wis.ntu.edu.sg/webexe88/owa/sso_login1.asp?t=1&p2=https://wis.ntu.edu.sg/pls/webexe88/srce_smain_s.Notice_O&extra=&pg=')
 
@@ -140,13 +139,14 @@ def new_window(username, court_number, timing):
         "/html/body/div/div/div[2]/table/tbody/tr/td/form/center[1]/table/tbody/tr/td/table/tbody/tr[5]/td/input[1]"))
     ).click()
 
-    count = 0
-    while True:
-        current_time = datetime.now().strftime("%H:%M:%S")
-        if current_time == MIDNIGHT:
-            break
-        timer.sleep(1)
-        count += 1  
+    if not debug:
+        count = 0
+        while True:
+            current_time = datetime.now().strftime("%H:%M:%S")
+            if current_time == MIDNIGHT:
+                break
+            timer.sleep(1)
+            count += 1  
     
     north_hill_court = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, 
         "//*[@id='top']/div/section[2]/div/div/p/table/tbody/tr/td[2]/form/ul/li[4]/table[2]/tbody/tr[1]/td/input"))
@@ -156,11 +156,17 @@ def new_window(username, court_number, timing):
     element = COURT_TO_XPATH[key]
     court_button = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, 
         "//*[@id='top']/div/section[2]/div/div/p/table/tbody/tr/td[2]/form/table[2]/tbody/{}/input".format(element)))
-    ).click()      
+    ).click()   
 
-    confirm_book_button = WebDriverWait(driver, 65).until(EC.element_to_be_clickable((By.XPATH, 
-        "//*[@id='top']/div/section[2]/div/div/p/table/tbody/tr/td[2]/form/input[18]"))
-    ).click()  
+    try:
+        import pyautogui
+        confirm_book_button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, 
+            "//*[@id='top']/div/section[2]/div/div/p/table/tbody/tr/td[2]/form/input[18]"))
+        ).click()
+        timer.sleep(2)
+        pyautogui.alert("Court booked. Click OK to exit.")
+    except UnexpectedAlertPresentException as e:
+        pyautogui.alert("Error in booking. "+ str(e))
 
 def main():
     root = tk.Tk()
@@ -170,6 +176,4 @@ def main():
     app.mainloop()
     
 if __name__ == "__main__":
-    #from pathos.helpers import freeze_support
-    #freeze_support()
     main()
